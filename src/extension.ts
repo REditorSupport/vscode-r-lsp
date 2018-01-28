2d5b63f83acf40151827a04682bef12062cdf898
YukiUeda <ikuyadeu@yahoo.co.jp>
2018-01-28 18:48:55 +0900

    apply gitignore

--- src/extension.ts
+++ src/extension.ts
@@ -15,7 +15,7 @@ export function activate(context: ExtensionContext) {
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
 	let serverOptions: ServerOptions = {
-		run : { command: "R", args: runArgs },
+		run : { command: "R", args: debugArgs },
 		debug: { command: "R", args: debugArgs }
 	}
 
