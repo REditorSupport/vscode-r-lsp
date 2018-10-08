72de388afee2eecff08271331c5a7cd2c698aed0
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2018-10-08 17:39:13 +0900

    fix performance

--- src/extension.ts
+++ src/extension.ts
@@ -1,70 +1,71 @@
-'use strict';
+"use strict";
 
-import { workspace, ExtensionContext, window } from 'vscode';
-import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient';
-import { Socket, createServer } from 'net';
-const cp = require("child_process");
+import cp = require("child_process");
+import { AddressInfo, createServer, Socket } from "net";
+import { ExtensionContext, window, workspace } from "vscode";
+import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from "vscode-languageclient";
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-	// The server is implemented in node
-	// let serverModule = context.asAbsolutePath(path.join('R'));
-	// The debug options for the server
-	const serverOptions: ServerOptions = function () {
-		return new Promise((resolve, reject) => {
-			let childProcess
-			const server = createServer(socket => {
-				// When the language server connects, grab socket, stop listening and resolve
-				// this.socket = socket
-				server.close()
-				resolve({ reader: socket, writer: socket })
-			})
+    // The server is implemented in node
+    // let serverModule = context.asAbsolutePath(path.join('R'));
+    // The debug options for the server
+    const serverOptions: ServerOptions = function foo() {
+        return new Promise((resolve, reject) => {
+            let childProcess;
+            const server = createServer((socket) => {
+                // When the language server connects, grab socket, stop listening and resolve
+                // this.socket = socket
+                server.close();
+                resolve({ reader: socket, writer: socket });
+            });
 
-			server.listen(0, '127.0.0.1', () => {
-				let port = server.address().port
-				let runArgs = ["--quiet", "--slave", "-e", `languageserver::run(port=${port})`];
-				let debugArgs = ["--quiet", "--slave", "-e", `languageserver::run(debug=TRUE, port=${port})`];
-				// Once we have a port assigned spawn the Language Server with the port
-				childProcess = cp.spawn(getRpath(), runArgs)
-				childProcess.stderr.on('data', (chunk: Buffer) => {
-					console.error(chunk + '');
-				});
-				childProcess.stdout.on('data', (chunk: Buffer) => {
-					console.log(chunk + '');
-				});
-				return childProcess
-			})
-		})
-	}
+            server.listen(0, "127.0.0.1", () => {
+                const port = server.address() as AddressInfo;
+                const runArgs = ["--quiet", "--slave", "-e", `languageserver::run(port=${port})`];
+                const debugArgs = ["--quiet", "--slave", "-e", `languageserver::run(debug=TRUE, port=${port})`];
+                // Once we have a port assigned spawn the Language Server with the port
+                childProcess = cp.spawn(getRpath(), runArgs);
+                childProcess.stderr.on("data", (chunk: Buffer) => {
+                window.showErrorMessage(chunk + "");
+                });
+                childProcess.stdout.on("data", (chunk: Buffer) => {
+                window.showInformationMessage(chunk + "");
 
-	let clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{scheme: 'file', language: 'r'}],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contain in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/*.r')
-		}
-	}
+                });
+                return childProcess;
+            });
+        });
+    };
 
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		'rlangsvr',
-		'Language Server R',
-		serverOptions,
-		clientOptions);
+    const clientOptions: LanguageClientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [{scheme: "file", language: "r"}],
+        synchronize: {
+            // Notify the server about file changes to '.clientrc files contain in the workspace
+            fileEvents: workspace.createFileSystemWatcher("**/*.r"),
+        },
+    };
 
-	// Start the client. This will also launch the server
-	client.start();
+    // Create the language client and start the client.
+    client = new LanguageClient(
+        "rlangsvr",
+        "Language Server R",
+        serverOptions,
+        clientOptions);
+
+    // Start the client. This will also launch the server
+    client.start();
 }
 
 export let config = workspace.getConfiguration("r");
 
 export function getRpath() {
-	let path = config.get("rpath.lsp") as string;
+    const path = config.get("rpath.lsp") as string;
     if (path !== "") {
         return path;
-	}
+    }
 
     if (process.platform === "win32") {
         return config.get("rterm.windows") as string;
