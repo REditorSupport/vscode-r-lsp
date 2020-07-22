2587d333ae2f20c9ae9ef459c25884f0f7cecf9e
Kun Ren <renkun@outlook.com>
2020-07-22 17:04:14 +0800

    Use unified output channel

--- src/extension.ts
+++ src/extension.ts
@@ -3,13 +3,13 @@ import { LanguageClient, LanguageClientOptions, StreamInfo } from 'vscode-langua
 import * as net from 'net';
 import * as url from 'url';
 import { getRPath } from './util'
-import { ExtensionContext, workspace, Uri, TextDocument, WorkspaceConfiguration, OutputChannel, window } from 'vscode';
+import { ExtensionContext, workspace, Uri, TextDocument, WorkspaceConfiguration, OutputChannel, window, WorkspaceFolder, DocumentSelector } from 'vscode';
 import os = require('os');
 
 let defaultClient: LanguageClient;
 let clients: Map<string, LanguageClient> = new Map();
 
-async function createClient(config: WorkspaceConfiguration, cwd: string, outputChannel: OutputChannel): Promise<LanguageClient> {
+async function createClient(config: WorkspaceConfiguration, cwd: string, workspaceFolder: WorkspaceFolder, outputChannel: OutputChannel): Promise<LanguageClient> {
     let client: LanguageClient;
 
     var debug = config.get("lsp.debug");
@@ -31,7 +31,10 @@ async function createClient(config: WorkspaceConfiguration, cwd: string, outputC
         console.log(str);
     }
 
-    const initArgs: string[] = config.get("lsp.args");
+    const options = { cwd: cwd, env: env };
+
+    let initArgs: string[] = [];
+    initArgs.push(config.get("lsp.args"));
     initArgs.push("--quiet", "--slave");
 
     const tcpServerOptions = () => new Promise<ChildProcess | StreamInfo>((resolve, reject) => {
@@ -55,7 +58,7 @@ async function createClient(config: WorkspaceConfiguration, cwd: string, outputC
             } else {
                 args = initArgs.concat(["-e", `languageserver::run(port=${port})`]);
             }
-            const childProcess = spawn(path, args, { cwd: cwd, env: env });
+            const childProcess = spawn(path, args, options);
             childProcess.stderr.on('data', (chunk: Buffer) => {
                 const str = chunk.toString();
                 console.log('R Language Server:', str);
@@ -71,27 +74,31 @@ async function createClient(config: WorkspaceConfiguration, cwd: string, outputC
         });
     });
 
+    let documentSelector;
+    if (workspaceFolder === undefined) {
+        documentSelector = [
+            { scheme: 'untitled', language: 'r' },
+            { scheme: 'untitled', language: 'rmd' }
+        ];
+    } else {
+        const pattern = `${workspaceFolder.uri.fsPath}/**/*`;
+        documentSelector = [
+            { scheme: 'file', language: 'r', pattern: pattern },
+            { scheme: 'file', language: 'rmd', pattern: pattern },
+        ];
+    }
+
     // Options to control the language client
     const clientOptions: LanguageClientOptions = {
         // Register the server for php documents
-        documentSelector: [
-            { scheme: 'file', language: 'r' },
-            { scheme: 'file', language: 'rmd' },
-            { scheme: 'untitled', language: 'r' },
-            { scheme: 'untitled', language: 'rmd' }
-        ],
+        documentSelector: documentSelector,
         uriConverters: {
             // VS Code by default %-encodes even the colon after the drive letter
             // NodeJS handles it much better
             code2Protocol: uri => url.format(url.parse(uri.toString(true))),
             protocol2Code: str => Uri.parse(str)
         },
-        synchronize: {
-            // Synchronize the setting section 'r' to the server
-            configurationSection: 'r.lsp',
-            // Notify the server about changes to R files in the workspace
-            fileEvents: workspace.createFileSystemWatcher('**/*.r')
-        },
+        workspaceFolder: workspaceFolder,
         outputChannel: outputChannel,
     };
 
@@ -103,7 +110,7 @@ async function createClient(config: WorkspaceConfiguration, cwd: string, outputC
         } else {
             args = initArgs.concat(["-e", `languageserver::run()`]);
         }
-        client = new LanguageClient('R Language Server', { command: path, args: args, options: { cwd: cwd, env: env } }, clientOptions);
+        client = new LanguageClient('R Language Server', { command: path, args: args, options: options }, clientOptions);
     } else {
         client = new LanguageClient('R Language Server', tcpServerOptions, clientOptions);
     }
@@ -116,6 +123,7 @@ export function activate(context: ExtensionContext) {
     const outputChannel: OutputChannel = window.createOutputChannel('R Language Server');
 
     async function didOpenTextDocument(document: TextDocument) {
+        outputChannel.appendLine(document.uri.toString());
         if (document.uri.scheme !== 'file' && document.uri.scheme !== 'untitled') {
             return;
         }
@@ -127,7 +135,7 @@ export function activate(context: ExtensionContext) {
         const uri = document.uri;
         // Untitled files go to a default client.
         if (uri.scheme === 'untitled' && !defaultClient) {
-            defaultClient = await createClient(config, os.homedir(), outputChannel);
+            defaultClient = await createClient(config, os.homedir(), undefined, outputChannel);
             defaultClient.start();
             return;
         }
@@ -138,7 +146,7 @@ export function activate(context: ExtensionContext) {
         }
 
         if (!clients.has(folder.uri.toString())) {
-            let client = await createClient(config, folder.uri.fsPath, outputChannel);
+            let client = await createClient(config, folder.uri.fsPath, folder, outputChannel);
             client.start();
             clients.set(folder.uri.toString(), client);
         }
